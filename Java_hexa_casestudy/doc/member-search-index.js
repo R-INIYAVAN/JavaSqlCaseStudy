14e2a1f3e7b6af3ae9bf35c2a76a7abd27f5559a
R-INIYAVAN <128072648+R-INIYAVAN@users.noreply.github.com>
2024-03-28 15:17:17 +0530

    Delete Java_hexa_casestudy/doc directory

--- Java_hexa_casestudy/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.hexaware.util","c":"DBUtil","l":"DBUtil()","u":"%3Cinit%3E()"},{"p":"com.hexaware.util","c":"DBUtil","l":"getDBConn()"},{"p":"com.hexaware.util","c":"DBUtil","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
